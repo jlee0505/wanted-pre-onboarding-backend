26c6af22a0552c52c930aa3b9b43742be15be8b0
jlee0505 <hj960322@gmail.com>
2024-08-01 12:10:51 +0900

    feat: 채용공고를 수정합니다.

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -17,7 +17,26 @@ exports.createJob = async (req, res) => {
   }
 };
 
-exports.updateJob = async (req, res) => {};
+exports.updateJob = async (req, res) => {
+  try {
+    const { jobId } = req.param;
+    const { position, reward, description, skills } = req.body;
+    const job = await Job.findByPk(jobId); // Sequelize.findByPk() [https://sequelize.org/docs/v6/core-concepts/model-querying-finders/#findbypk]
+
+    if (job) {
+      job.position = position;
+      job.reward = reward;
+      job.description = description;
+      job.skills = skills;
+      await job.save(); // Sequelize.save(); [https://sequelize.org/docs/v7/querying/update/#updating-a-row-using-modelsave]
+      res.json(job);
+    } else {
+      res.status(404).json({ error: 'Job not found.' });
+    }
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
 
 exports.deleteJob = async (req, res) => {};
 exports.getJobs = async (req, res) => {};
