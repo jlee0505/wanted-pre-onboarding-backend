efe36ac1c77b3cbdff175d007630b8f2e4cbccc2
jlee0505 <hj960322@gmail.com>
2024-08-01 12:42:19 +0900

    feat: 채용공고 목록을 가져옵니다.

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -38,5 +38,33 @@ exports.updateJob = async (req, res) => {
   }
 };
 
-exports.deleteJob = async (req, res) => {};
-exports.getJobs = async (req, res) => {};
+exports.deleteJob = async (req, res) => {
+  try {
+    const { jobId } = req.param;
+    const job = await Job.findByPk(jobId);
+
+    if (job) {
+      await job.destroy(); // [https://sequelize.org/docs/v7/querying/delete/#deleting-a-single-row]
+      res.status(204).send(); // 204 error [https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204]
+    } else {
+      res.status(404).json({ error: 'Job not found' });
+    }
+  } catch (e) {
+    res.status(505).json({ error: e.message });
+  }
+};
+
+exports.getJobs = async (req, res) => {
+  try {
+    const jobs = await Job.findAll({
+      // basic: [https://sequelize.org/docs/v7/querying/select-methods/] / in depth : [https://sequelize.org/docs/v7/querying/select-in-depth/]
+      include: {
+        model: Company,
+        attributes: ['name', 'country', 'region'],
+      },
+    });
+    res.json(jobs);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
