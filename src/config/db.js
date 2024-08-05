c943ee84348f9b3bb70a20764ee29a9a8cb2cb58
jlee0505 <hj960322@gmail.com>
2024-08-05 12:54:34 +0900

    chore: removed the accidentally derived js file

--- src/config/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sequelize_1 = require("sequelize");
-var sequelize = new sequelize_1.Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-});
-exports.default = sequelize;
